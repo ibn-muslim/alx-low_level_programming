cdaa81e5c910f6607fbdfea99edb54ee720ab849
Abdulmumin <abdulmuminabdulazeezyusuf@gmail.com>
2022-09-23 08:56:52 -0700

    more functions, more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,44 +1,35 @@
 #include "main.h"
 
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
- * Return: Always 0.
+ * print_number - prints # using _putchar function
+ * @n: the integer to print
+ *
+ * Return: void
  */
 
 void print_number(int n)
 {
-unsigned int z;
-int m, b;
-b = 10;
-if (n < 10 && n >= 0)
+int copy, nth, size = 1, ones = n % 10;
+n /= 10;
+copy = n;
+if (ones < 0)
 {
-_putchar (n + '0');
-}
-else if (n > -10 && n < 0)
-{
-n = n - 2 * n;
+ones *= -1, copy *= -1, n *= -1;
 _putchar('-');
-_putchar (n + '0');
 }
-else
+if (copy > 0)
 {
-if (n < 0)
+while (copy / 10 != 0)
 {
-n = n * -1;
-_putchar ('-');
+copy /= 10, size *= 10;
 }
-z = n;
-while (z / b > 9)
+while (size > 0)
 {
-b = b * 10;
-}
-while (b > 0)
-{
-m = z / b;
-z = z % b;
-_putchar (m + '0');
-b = b / 10;
+nth = n / size;
+_putchar('0' + nth);
+n -= nth * size;
+size /= 10;
 }
 }
+_putchar('0' + ones);
 }
